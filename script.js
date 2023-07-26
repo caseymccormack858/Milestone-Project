df5c4d4cc622e19dca9b2338e9b223fe13774c77
Casey McCormack <caseymccormack858@gmail.com>
2023-07-25 19:36:15 -0700

    var number counter

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ document.addEventListener("keydown", event => {
     if(event.key==="ArrowLeft"){moveLeft();}
     if(event.key==="ArrowRight"){moveRight();}
     });
+    var block = document.getElelemntById("block");
+var counter = 0;
+block.addEventListener('animationiteration',()
+= > {
+    var random = Math.floor(Math.random() *3);
+    left = random * 100;
+    block.style.left = left = "px";
+    counter++;
+});
