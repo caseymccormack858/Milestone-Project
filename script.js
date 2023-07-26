2843aabd2e8fe6574f6e91bdeb9f6dbb4e1793e9
Casey McCormack <caseymccormack858@gmail.com>
2023-07-25 19:29:31 -0700

    codefunction

---
+++ script.js
@@ -0,0 +1,8 @@
+codefunction moveLeft(){
+    left left =
+    parseInt(windoiw.getComputedStyle(character).getPropertyValue("Left"));
+    left -=100;
+    if(left>=0){
+    character.style.left = left + "px";
+    }
+}
